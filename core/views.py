b903a903672675c4c446c56da754bcd10d22ec59
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-09-14 15:58:33 -0400

    Add links

--- core/views.py
+++ core/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def splash(request):
     return render(request, 'splash.html', {})
 
+def apply(request):
+    return render(request, 'apply.html', {})
+
 def read(request):
     return render(request, 'read.html', {})
--- penntechreview/urls.py
+++ penntechreview/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import splash, read
+from core.views import splash, read, apply
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('read/', read, name='read'),
-    path('', splash, name='splash')
+    path('', apply, name='apply')
 ]
