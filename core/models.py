b77119fdc27c3992d35948a96f3665bac5dc2f63
Jerry <jerry.z.lu@gmail.com>
2018-10-14 14:01:23 -0400

    Edited models

--- core/models.py
+++ core/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class TeamMember(models.Model):
+class Member(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey(User, primary_key=True)
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    subtitle = models.CharField(max_length=200)
+    body = models.TextField()
     author = models.ForeignKey(TeamMember, on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
 
