4310962794393fc51e569ce32f630336dffad7c2
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-10-29 21:57:12 -0400

    Add dynamic rendering

--- core/models.py
+++ core/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Member(models.Model):
+
+class Author(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey(User, primary_key=True)
+    user = models.ForeignKey(User, primary_key=True, on_delete=models.CASCADE)
+   
+    def __str__(self):
+        return self.name
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
     body = models.TextField()
-    author = models.ForeignKey(Member, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     pub_date = models.DateTimeField()
 
     def __str__(self):
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from core.models import Post
+import markdown2
 
 def splash(request):
     return render(request, 'splash.html', {})
@@ -7,4 +9,7 @@ def apply(request):
     return render(request, 'apply.html', {})
 
 def read(request):
-    return render(request, 'read.html', {})
+    post_id = request.GET['post']
+    post = Post.objects.get(pk=int(post_id))
+    post.body = markdown2.markdown(post.body)
+    return render(request, 'read.html', {'post': post})
